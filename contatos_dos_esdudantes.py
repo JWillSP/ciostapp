9df245c76dd0d96e1d5346b6438e7da804a2bd53
jwdeploy <137692546+jwdeploy@users.noreply.github.com>
2023-09-02 17:07:06 -0300

    take picture

---
+++ contatos_dos_esdudantes.py
@@ -0,0 +1,105 @@
+import streamlit as st
+from datetime import datetime, timedelta
+import pandas as pd
+import base64
+import io
+import cv2
+import numpy as np
+from PIL import Image
+
+def back_image(image):
+    xpl2 = image.convert('RGB')
+    img_buf = io.BytesIO()
+    xpl2.save(img_buf, format='JPEG', quality=95)
+    value_got = img_buf.getvalue()
+    encodado = base64.b64encode(value_got).decode('ascii')
+    return encodado
+
+def b64pic(image_pil):
+    return 'data:image/jpg;base64,' + back_image(image_pil)
+
+
+def get_image(upload_first=False):
+    options = ["Câmera", "Upload de foto"]
+    if upload_first:
+        options.reverse()
+
+    choice = st.radio("Escolha o método de aquisição da imagem", options)
+    if choice == "Câmera":
+        file = st.camera_input(
+            label="Tire a foto clicando no botão abaixo"
+        )
+    else:
+        file = st.file_uploader("Faça upload de uma imagem da sua galeria", type=["png", "jpg", "jpeg"])
+
+    def correct_image_orientation(image):
+        """Corrige a orientação de uma imagem com base em seus metadados EXIF."""
+        try:
+            exif_data = image._getexif()
+            orientation_tag = 274  # The orientation tag ID in EXIF data
+            if exif_data is not None and orientation_tag in exif_data:
+                orientation = exif_data[orientation_tag]
+                if orientation == 2:
+                    image = image.transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 3:
+                    image = image.rotate(180)
+                elif orientation == 4:
+                    image = image.rotate(180).transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 5:
+                    image = image.rotate(-90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 6:
+                    image = image.rotate(-90, expand=True)
+                elif orientation == 7:
+                    image = image.rotate(90, expand=True).transpose(Image.FLIP_LEFT_RIGHT)
+                elif orientation == 8:
+                    image = image.rotate(90, expand=True)
+        except (AttributeError, KeyError, IndexError):
+            pass
+        return image
+
+    if file:
+        PIL_img = Image.open(file)
+        PIL_img = correct_image_orientation(PIL_img)
+        img = np.array(PIL_img)
+        gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        haar_cascade = cv2.CascadeClassifier('Haarcascade_frontalface_default.xml')
+        faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9)
+        for (x, y, w, h) in faces_rect:
+            factor = 1.7
+            new_w = int(w*factor)
+            new_h = int(h*factor)
+            hfivefourthdif = int((5/4)*new_w) - new_h
+            desloc_x = int(0.06*w)
+            desloc_y = int(0.06*h)
+            dif_w = int((new_w - w)/2)
+            dif_h = int((new_h - h)/2)
+            cv2.rectangle(
+                img,
+                (x - dif_w, y - dif_h - desloc_y),
+                (x + w + dif_w, y + h + dif_h - desloc_y + hfivefourthdif),
+                (0, 200, 0),
+                2
+            )
+            # cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            facezoom = 0.65
+            new_dif_h = int(dif_h*facezoom)
+            sq_x1 = x - new_dif_h
+            sq_y1 = y - new_dif_h - desloc_y
+            sq_x2 = x + h + new_dif_h
+            sq_y2 = y + h + new_dif_h - desloc_y
+            cv2.rectangle(
+                img,
+                (sq_x1, sq_y1),
+                (sq_x2, sq_y2),
+                (200, 0, 0),
+            )
+            break
+
+        st.image(img, caption='Rosto encontrado', use_column_width=True)
+        cinco_quartos = PIL_img.crop((x - dif_w, y - dif_h - desloc_y, x + w + dif_w, y + h + dif_h - desloc_y + hfivefourthdif))
+        squared = PIL_img.crop((sq_x1, sq_y1, sq_x2, sq_y2))
+        resized_img = cv2.resize(np.array(squared), (64, 64), interpolation=cv2.INTER_AREA)
+        main_resized_img = cv2.resize(np.array(cinco_quartos), (160, 200), interpolation=cv2.INTER_AREA)
+        b64_pic = b64pic(Image.fromarray(main_resized_img))
+        b64_thumb = b64pic(Image.fromarray(resized_img))
+        return b64_pic, b64_thumb
---
+++ pages/tirar_foto_do_estudante.py
@@ -0,0 +1,223 @@
+import pandas as pd
+import streamlit as st
+from pymongo import MongoClient
+from PIL import Image
+import base64
+import io
+from contatos_dos_esdudantes import get_image
+
+try:
+    dbcred = st.secrets["dbcred"]
+    db1 = st.secrets["db1"]
+    pics = st.secrets["pics"]
+    creds = st.secrets["creds"]
+    colec1 = st.secrets["colec1"]
+    secret_key = st.secrets["secret_key"]
+
+except FileNotFoundError:
+    import os
+    dbcred = os.environ['dbcred']
+    db1 = os.environ["db1"]
+    pics = st.secrets["pics"]
+    creds = st.secrets["creds"]
+    colec1 = os.environ["colec1"]
+    secret_key = os.environ["secret_key"]
+
+cluster = MongoClient(dbcred)
+db = cluster[db1]
+
+def get_all_have_pic():
+    collection = db[pics]
+    all_have_pic = collection.distinct('_id')
+    return all_have_pic
+st.write(len(get_all_have_pic()))
+
+def fetch_image(matrícula: str) -> Image.Image:
+    col_pix = db[pics]
+    std_pic = col_pix.find_one({'_id': matrícula})
+    if std_pic:
+        return std_pic['pic']
+    else:
+        return None
+
+def upload_pic_and_thumb(rm, pic, thumb):
+    pic_ = pic[pic.find(',') + 1:]
+    thumb_ = thumb[thumb.find(',') + 1:]
+    dic_ = {
+        "_id": rm,
+        "pic": pic_,
+        "thumb": thumb_,
+        "ext": "jpg",
+        "extthumb": "jpg"
+    }
+    collection = db[pics]
+    update_result = collection.update_one(
+        {
+            "_id": rm
+        },
+        {
+            "$set": dic_
+        },
+        upsert=True
+    )
+
+    if update_result.upserted_id:
+        return f"Fotos enviadas! Com id: {update_result.upserted_id}"
+    elif update_result.modified_count:
+        return "Fotos atualizadas com sucesso!"
+    else:
+        return "Erro ao enviar as fotos."
+
+
+@st.cache_data(ttl=3600)
+def get_data_at_db():
+    collection1 = db[colec1] 
+    all_stds = pd.DataFrame(list(collection1.find()))
+    return all_stds
+
+
+picscolection = db[pics] 
+
+# df7x = get_data_at_db()
+
+# @st.cache_data(ttl=3600)
+# def get_local():
+#     collection = db[creds] 
+#     all_creds = pd.DataFrame(list(collection.find()))
+#     return all_creds
+
+# local = get_local()
+
+# df7x['local'] = df7x['matrícula'].apply(lambda x: local.set_index('matrícula')['transporte'].to_dict().get(x, 'SEDE DO MUNICÍPIO'))
+
+# def gen_button_list(lista=[]):
+
+
+
+def main(user, logout):
+    st.title("TIRAR FOTOS DOS ESTUDANTES")
+    all_stds = get_data_at_db()
+    mapping_stds = {
+        matrícula: ' - '.join(
+            (nome, matrícula, turma)
+        ) 
+        for nome, matrícula, turma
+        in zip(
+            all_stds['estudante'],
+            all_stds['matrícula'],
+            all_stds['turma']
+        )
+    }
+    all_stds['buscador'] = all_stds['matrícula'].map(mapping_stds)
+    # selecionar se vai exibir os estudantes sem fotos ou com fotos
+    sem_pic = all_stds[~all_stds['matrícula'].isin(get_all_have_pic())]
+    com_pic = all_stds[all_stds['matrícula'].isin(get_all_have_pic())]
+    exibir = st.radio('EXIBIR:', ['Estudantes SEM fotos', 'Estudantes COM fotos'])
+    col_a, col_b, col_c = st.columns(3)
+    with col_a:
+        st.metric(label='Estudantes SEM fotos', value=len(sem_pic))
+    with col_b:
+        st.metric(label='Estudantes COM fotos', value=len(com_pic))
+    with col_c:
+        st.metric(label='Total de Estudantes', value=len(all_stds))
+
+    if exibir == 'Estudantes SEM fotos':
+        all_students = sem_pic
+    else:
+        all_students = com_pic
+
+    def select_turmas():
+        todas_turmas = all_students['turma'].unique().tolist()
+        todas_turmas.sort()
+        todas_turmas.insert(0, 'TURMAS')
+        turma = st.selectbox('Selecione a turma:', todas_turmas)
+        if turma == 'TURMAS':
+            return None
+        estudantes_da_turma = all_students[all_students['turma'] == turma]
+        marcado = st.radio('Estudante encontrado:', estudantes_da_turma['buscador'].values.tolist())
+        selected_student = estudantes_da_turma[estudantes_da_turma['buscador'] == marcado]
+        std = selected_student.iloc[0]
+        student_id = std.matrícula
+        student_name_db = std.estudante
+        return std.buscador, student_name_db, student_id
+
+    
+
+    def search_student():
+        st.markdown("## BUSCAR ESTUDANTE")
+        student_name = st.text_input("Digite o nome do estudante, matrícula ou turma:", value='digite aqui')
+        filtered_students = all_students[all_students['buscador'].str.contains(student_name, case=False)]
+        if len(filtered_students) == 0:
+            st.write('')
+            return None
+        elif len(filtered_students) == 1:
+            marcado = st.radio('Estudante encontrado:', filtered_students['buscador'].values.tolist())
+            selected_student = all_students[all_students['buscador'] == marcado]
+            std = selected_student.iloc[0]
+            student_id = std.matrícula
+            student_name_db = std.estudante
+            return std.buscador, student_name_db, student_id
+        elif len(filtered_students) > 20:
+            st.write("Muitos resultados, refine sua busca!")
+            return None
+        else:
+            st.write("Estudantes encontrados:")
+            marcado = st.radio('marque o aluno correto: ', ['Marque alguém!'] + filtered_students['buscador'].values.tolist())
+
+            selected_student = all_students[all_students['buscador'] == marcado]
+            try: 
+                std = selected_student.iloc[0]
+            except IndexError:
+                return None
+            student_id = std.matrícula
+            student_name_db = std.estudante
+            return std.buscador, student_name_db, student_id
+            
+    metodo = st.radio('Selecione o método de busca: ', ['Turma', 'Alunos Avulsos'])
+    if metodo == 'Turma':
+        result = select_turmas()
+
+    if metodo == 'Alunos Avulsos':
+        result = search_student()
+
+    if result:
+        received = fetch_image(result[2])
+        if received is None:
+            st.write('Não há foto cadastrada para este estudante!')
+            try: 
+                pic, thumb = get_image()
+            except TypeError:
+                return None
+            st.write('principal')
+            st.markdown(f'<img src="{pic}" />', unsafe_allow_html=True)
+            st.success(str(len(f'~<img src="{pic}" />')//1024) + ' KB')
+            st.write('miniatura')
+            st.markdown(f'<img src="{thumb}" />', unsafe_allow_html=True)
+            st.success(str(len(f'~<img src="{thumb}" />')//1024) + ' KB')
+            if st.button('Enviar foto para o banco de dados'):
+                upload_pic_and_thumb(result[2], pic, thumb)
+                st.success('Foto enviada com sucesso!')
+        else:
+            st.write(result[0])
+            st.markdown(f'<img src="data:image/jpg;base64,{received}" />', unsafe_allow_html=True)
+            # image_data = base64.b64decode(bytes(received, 'utf-8'))
+            # image = Image.open(io.BytesIO(image_data))
+            # print(image.size)
+            # st.image(image, caption=f'{result[1]} - {result[2]}', width=250)
+            st.write('')
+            st.write('Atualizar foto')
+            try:
+                pic, thumb = get_image(upload_first=True)
+            except TypeError:
+                return None
+            st.write('principal')
+            st.markdown(f'<img src="{pic}" />', unsafe_allow_html=True)
+            st.success(str(len(f'~<img src="{pic}" />')//1024) + ' KB')
+            st.write('miniatura')
+            st.markdown(f'<img src="{thumb}" />', unsafe_allow_html=True)
+            st.success(str(len(f'~<img src="{thumb}" />')//1024) + ' KB')
+            if st.button('Enviar foto para o banco de dados'):
+                upload_pic_and_thumb(result[2], pic, thumb)
+                st.success('Foto enviada com sucesso!')
+
+main(user='user', logout='logout')
